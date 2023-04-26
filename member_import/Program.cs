ca480c4130c9612e5d363c6197722e06fac26a6d
Sahand Ghaemi <sahandghaemi98@outlook.com>
2023-04-26 14:28:10 -0600

    updated

--- member_import/Program.cs
+++ member_import/Program.cs
@@ -8,10 +8,12 @@ class Program
 {
     static void Main()
     {
-        string excelFilePath = "path/to/your/excel/file.xlsx";
-        string connectionString = "Data Source=(local);Initial Catalog=YourDatabaseName;Integrated Security=True"; // Replace with your actual connection string
+        string excelFilePath = @"C:\Users\963193\Documents\practice\QNXT_Training_Member_Inbound_File.xlsx";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QNXT_Test;Integrated Security=True";
 
         // Read data from Excel file using EPPlus
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
         using (var package = new ExcelPackage(new FileInfo(excelFilePath)))
         {
             ExcelWorksheet worksheet = package.Workbook.Worksheets[0]; // Assuming the data is in the first sheet of the Excel file
@@ -27,30 +29,30 @@ class Program
                     string memberId = GenerateMemberId();
 
                     // Extract data from the row
-                    string firstname = worksheet.Cells[row, 2].Value?.ToString();
-                    string lastname = worksheet.Cells[row, 3].Value?.ToString();
-                    string middlename = worksheet.Cells[row, 4].Value?.ToString();
-                    DateTime dob = Convert.ToDateTime(worksheet.Cells[row, 5].Value);
-                    string gender = worksheet.Cells[row, 6].Value?.ToString();
-                    string addr1 = worksheet.Cells[row, 7].Value?.ToString();
-                    string addr2 = worksheet.Cells[row, 8].Value?.ToString();
-                    string city = worksheet.Cells[row, 9].Value?.ToString();
-                    string zip = worksheet.Cells[row, 10].Value?.ToString();
-                    string ssn = worksheet.Cells[row, 11].Value?.ToString();
-                    string createId = "YourSQLUsername"; // Replace with your actual SQL username
+                    string firstname = worksheet.Cells[row, 1].Value != null ? worksheet.Cells[row, 1].Value.ToString() : "";
+                    string lastname = worksheet.Cells[row, 2].Value != null ? worksheet.Cells[row, 2].Value.ToString() : "";
+                    string middlename = worksheet.Cells[row, 3].Value != null ? worksheet.Cells[row, 3].Value.ToString() : "";
+                    string fullname = firstname + " " + middlename + " " + lastname;
+                    DateTime dob = Convert.ToDateTime(worksheet.Cells[row, 4].Value);
+                    string gender = worksheet.Cells[row, 5].Value != null ? worksheet.Cells[row, 5].Value.ToString() : "";
+                   gender = gender.Substring (0, 1).ToUpper();
+                    string addr1 = worksheet.Cells[row, 6].Value != null ? worksheet.Cells[row, 6].Value.ToString() : "";
+                    string addr2 = worksheet.Cells[row, 7].Value != null ? worksheet.Cells[row, 7].Value.ToString() : "";
+                    string city = worksheet.Cells[row, 8].Value != null ? worksheet.Cells[row, 8].Value.ToString() : "";
+                    string zip = worksheet.Cells[row, 9].Value != null ? worksheet.Cells[row, 9].Value.ToString() : "";
+                    string ssn = worksheet.Cells[row, 10].Value != null ? worksheet.Cells[row, 10].Value.ToString() : "";
+                    string createId = "DBO"; 
                     DateTime createDate = DateTime.Now;
-                    string updateId = "YourSQLUsername"; // Replace with your actual SQL username
+                    string updateId = "DBO"; 
                     DateTime lastUpdate = DateTime.Now;
 
                     // Insert data into SQL Server
-                    string query = @"INSERT INTO YourTableName (MemberId, Firstname, Lastname, MiddleName, DOB, Gender, Addr1, Addr2, City, Zip, SSN, CreateID, CreateDate, UpdateID, LastUpdate)
-                                     VALUES (@MemberId, @Firstname, @Lastname, @MiddleName, @DOB, @Gender, @Addr1, @Addr2, @City, @Zip, @SSN, @CreateID, @CreateDate, @UpdateID, @LastUpdate)";
+                    string query = @"INSERT INTO dbo.tzct_trn_member_import (MemberId, FullName, DOB, Gender, Addr1, Addr2, City, Zip, SSN, CreateID, CreateDate, UpdateID, LastUpdate)
+                                     VALUES (@MemberId, @FullName, @DOB, @Gender, @Addr1, @Addr2, @City, @Zip, @SSN, @CreateID, @CreateDate, @UpdateID, @LastUpdate)";
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
                         command.Parameters.AddWithValue("@MemberId", memberId);
-                        command.Parameters.AddWithValue("@Firstname", firstname);
-                        command.Parameters.AddWithValue("@Lastname", lastname);
-                        command.Parameters.AddWithValue("@MiddleName", middlename);
+                        command.Parameters.AddWithValue("@FullName", fullname);
                         command.Parameters.AddWithValue("@DOB", dob);
                         command.Parameters.AddWithValue("@Gender", gender);
                         command.Parameters.AddWithValue("@Addr1", addr1);
